7bdbca648e3c9bb5fb55de338464023ff0b5cadd
Fardoe <dfardoe@ad.cityofwpg.org>
2018-12-14 10:08:35 -0600

    Draw the basic grid

---
+++ SudokuSolver.py
@@ -0,0 +1,21 @@
+def print_grid(sudoku_matrix):
+    # Print header row
+    print(u'\u2554' + u'\u2550' * 7 + u'\u2566' + u'\u2550' * 7 + u'\u2566' + u'\u2550' * 7 + u'\u2557')
+
+    # Print first three rows
+
+    # Print separator row
+    print(u'\u2560' + u'\u2550' * 7 + u'\u256C' + u'\u2550' * 7 + u'\u256C' + u'\u2550' * 7 + u'\u2563')
+
+    # Print next three rows
+
+    # Print separator row
+    print(u'\u2560' + u'\u2550' * 7 + u'\u256C' + u'\u2550' * 7 + u'\u256C' + u'\u2550' * 7 + u'\u2563')
+
+    # Print last three rows
+
+    # Print footer row
+    print(u'\u255A' + u'\u2550' * 7 + u'\u2569' + u'\u2550' * 7 + u'\u2569' + u'\u2550' * 7 + u'\u255D')
+    
+
+print_grid(None)
